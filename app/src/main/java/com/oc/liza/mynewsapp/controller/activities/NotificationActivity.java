fa72c3201ff7fcc000e9692ffe0dcc9d73427f5d
Eliza13210 <elisabetboulanger@gmail.com>
2018-12-23 08:43:14 +0100

    Created layout NotificationActivity

--- app/src/main/java/com/oc/liza/mynewsapp/controller/activities/NotificationActivity.java
+++ app/src/main/java/com/oc/liza/mynewsapp/controller/activities/NotificationActivity.java
@@ -1,15 +1,49 @@
 package com.oc.liza.mynewsapp.controller.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 
 import com.oc.liza.mynewsapp.R;
 
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class NotificationActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.query)
+    EditText query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        ButterKnife.bind(this);
+        initNotification();
+    }
+
+    private void initNotification() {
+
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        query.setSelection(0);
+        Objects.requireNonNull(this.getWindow()).setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 }
